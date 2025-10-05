f71d3a84eccbbd2091e699f151a4ffb0340a522c
lahupande01 <lahupande01@gmail.com>
2025-10-05 07:45:06 +0530

    added code for home and login, also made changes in reg page

---
+++ src/login/loginPage.java
@@ -0,0 +1,10 @@
+package login;
+
+public class loginPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/reg/RegostrationDemo.java
+++ src/reg/RegostrationDemo.java
@@ -5,7 +5,9 @@ public class RegostrationDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("reg demo");
-
+		System.out.println("hi demo");
+		System.out.println("hello JOBITO");
+		
 	}
 
 }
