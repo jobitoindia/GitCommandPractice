288414598124e9818f3fbc2f5df17797add2f764
lahupande01 <lahupande01@gmail.com>
2025-10-06 07:25:01 +0530

    rushikesh added code validating the products and price

---
+++ src/home/ValidateProducts.java
@@ -0,0 +1,10 @@
+package home;
+
+public class ValidateProducts {
+	
+	public static void main(String[] args) {
+		System.out.println("i am adding code for listing the products");
+		
+	}
+
+}
---
+++ src/home/ValidteThePrice.java
@@ -0,0 +1,12 @@
+package home;
+
+public class ValidteThePrice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("validate the price");
+
+	}
+
+}
